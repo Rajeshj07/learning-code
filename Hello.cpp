c1f1e9f5e72953ea4e59d7c31603c851e4dd80c4
Rajeshj07 <rjj7406@gmail.com>
2025-10-17 19:02:33 +0530

    Modified

--- Hello.cpp
+++ Hello.cpp
@@ -1,7 +1,7 @@
 #include<istream>
 
 int main()  {
-    printf("Name:- Rajesh Jagdish Jadhav/n");
-    printf("Email:- rjj7406@gmail.com/n")
+    printf("Name:- Rajesh Jagdish Jadhav\n");
+    printf("Email:- rjj7406@gmail.com\n");
     return 0;
 }
