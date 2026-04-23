f2fb509153bf0188432f7c4d5d7a508deceb3a8c
Rajeshj07 <rjj7406@gmail.com>
2026-04-23 13:28:42 +0530

    Implement factorial calculation in Code.c
    Fix factorial calculation and add user input.

--- Code.c
+++ Code.c
@@ -1,7 +1,17 @@
-#include <stdio.h> 
+#include <stdio.h>
+int main() { 
+    int fact, n, a;
+    fact = 0
+        printf("Enter the number for factorial: ");
+        scanf("%d", &n)
 
-int main() {
-    printf("Hello, World!\n");
-    printf("Name- Rajesh Jagdish Jadhav");
+    for(a=1; a<=n; a++)
+            {
+                fact = fact * a;
+            }
+    printf("The factorial is %d", fact);
+
+    swap();
+
     return 0;
 }
